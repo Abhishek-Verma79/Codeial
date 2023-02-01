9bbc938e234dff2bd162f27d078cf573f517a738
Abhishek Verma <wrmaabhishek@gmail.com>
2023-02-01 19:08:07 +0530

    created sign-in and sign-up page and sign-up's action

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req,res){
    return res.render('users',{
       title : "users",
@@ -5,3 +7,49 @@ module.exports.profile = function(req,res){
    })
 }
 
+//render the sign up page
+module.exports.signUp = function(req,res){
+   return res.render('user_sign_up',{
+      title : "Codeial | Sign up"
+   })
+}
+
+//render the sign in page
+module.exports.signIn = function(req,res){
+   return res.render('user_sign_in',{
+      title : 'Codeial | Sign in'
+   })
+}
+
+// Get the sign up data
+module.exports.create = function(req,res){
+   if(req.body.password != req.body.confirm_password){
+      console.log("password and confirm password aren't same!");
+      return res.redirect('back');
+   }
+
+   User.findOne({email: req.body.email},function(err,user){
+      if(err){
+         console.log("Error in finding user in signing up!");
+         return;
+      }
+
+      if(!user){
+         User.create(req.body,function(err,user){
+            if(err){
+               console.log("Error in creating user while signing up!");
+               return;
+            }
+            return res.redirect('/users/sign-in');
+         });
+      }else{
+         return res.redirect('back');
+      }
+
+   });
+}
+
+// sign in and create the session for user
+module.exports.createSession = function(req,res){
+   //TODO later
+}
