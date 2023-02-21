f4cf17de8c69027b4db266b05a28174d6ced5861
Abhishek Verma <wrmaabhishek@gmail.com>
2023-02-21 20:05:47 +0530

    Display posts with username after user Authentication

--- routes/post.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const postShowController = require("..//controllers/post_controller");
-router.get('/show',postShowController.showPost)
-
-module.exports = router;
